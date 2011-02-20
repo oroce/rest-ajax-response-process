8577846e459096ea5ff3af32ff6a0027dcaa6e8e
oroce <robert@oroszi.net>
2011-02-20 21:29:29 +0100

    modified:   script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+	$(document).ajaxSuccess(function( e, xhr, opt ){
+	
+			
+	
+	});
+	
+	$(document).ajaxError(function( e, xhr, opt, err ){
+	
+		
+	
+	});
+
+
+
+
+
+
+
+});
